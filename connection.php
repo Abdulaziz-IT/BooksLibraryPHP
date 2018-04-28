cb515f1b84b10c08150cdbd14391df151da66afa
Abdulaziz Alshememry <abdulaziz.wal@gmail.com>
2018-04-28 16:05:17 +0000

    Last edit

--- connection.php
+++ connection.php
@@ -1,12 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "abdulazizwaleed";
-$password = "";
-$database = "book_store";
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$servername = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 
 $conn = new mysqli($servername, $username, $password, $database);
 
 if($conn->connect_error) {
+    echo substr($url["path"], 1);
     echo "Failed to connect to database!";
     die("Connection failed: ");
 }
